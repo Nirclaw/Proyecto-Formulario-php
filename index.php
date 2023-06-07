f1da8679501264bd080926870d8e0d3d964f6de6
Nirclaw <caicedonicolas150@gmail.com>
2023-06-07 17:23:48 -0500

    feat: :tada: Inicio de Proyecto
    Se realiza parte del maquetado de la pagina web aun falta desarrollo de logica

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./css/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <form action="">
+            <div id="encabezado">
+                <input type="text" name="nombre" id="nombre" placeholder="nombre">
+                <img src="" alt="logo" srcset="https://media.licdn.com/dms/image/D563DAQFas8vErYi8iA/image-scale_191_1128/0/1681268367946?e=1686783600&v=beta&t=AxWQwwzzQ5dfcbg7RTf3_LAWFZUaEMWYY8pRfOr3MlM">
+                <input type="text" name="apellido" id="apellido" placeholder="edad">
+                <input type="text" name="edad" id="edad" placeholder="edad">
+                <input type="text" name="direccion" id="direccion" placeholder="direccion">
+                <input type="text" name="email" id="email" placeholder="email">
+            </div>
+            <div id="botonera">
+
+            
+            </div>
+
+
+        </form>
+    </div>
+</body>
+
+</html>
