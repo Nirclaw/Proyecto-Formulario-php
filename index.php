44c8f9f1ad67179705ba8637ef4bd9057c793e73
Nicolas <112518653+Nirclaw@users.noreply.github.com>
2023-06-09 04:10:15 -0500

    feat: :sparkles: Se agrega funcionalidad completa de crud
    Se corrigen errores y posibles bugs, actualmente el codigo cuenta con validaciones y el crud completo funcional

--- index.php
+++ index.php
@@ -5,26 +5,16 @@ $baseUrl = 'https://648233f729fa1c5c5032b72c.mockapi.io/';
 //metodo para crear usuarios, 
 function crearUsuario($datos)
 {
+
     global $baseUrl;
     $url = $baseUrl . "usuarios";
-
-    $json = array(
-        "nombre" => $datos['nombre'],
-        "apellido" => $datos['apellido'],
-        "direccion" => $datos['direccion'],
-        "edad" => $datos["edad"],
-        "email" => $datos['email'],
-        "hora" => $datos['Horario'],
-        "trainer" => $datos['trainer__'],
-        "cedula" => $datos['cedula'],
-        "team" => $datos['team']
-    );
+    var_dump($datos);
     //configuracion para realizar un POST
     $options = array(
         "http" => array(
             "method" => "POST",
             "header" => "Content-type: application/json",
-            "content" => json_encode($json)
+            "content" => json_encode($datos)
         )
     );
 
@@ -80,9 +70,10 @@ function validarexistencia($dato)
     for ($i = 0; $i < count($usuario); $i++) {
         if ($dato == $usuario[$i]['cedula']) {
             echo $usuario[$i]['cedula'];
+            return true;
         }
-        return true;
     }
+    return false;
 }
 
 function buscarid($cedula)
@@ -101,35 +92,103 @@ function buscarid($cedula)
         }
     }
 }
-$nombretraer = buscarid($_POST['cedula']);
 
-var_dump($nombretraer);
+function borrarCliente($cedula)
+{
+    $cliente = buscarid($cedula);
+    global $baseUrl;
+    $url = $baseUrl . "usuarios/" . $cliente['id'];
 
+    $options = array(
+        "http" => array(
+            "method" => "DELETE"
+        )
+    );
 
+    $context = stream_context_create($options);
 
-switch ($_POST['btn']) {
+    $enviar = file_get_contents($url, false, $context);
 
-    case 'borrar':
-        echo $_POST['btn'];
-
-        break;
-
-    case 'guardar':
-        if (in_array("", $_POST)) {
-            echo "Debes completar todos los campos";
-        } else {
-            validarexistencia($_POST['cedula']) ?  print "el usuario ya existe" : crearUsuario($_POST);
-        }
-        break;
-    case 'editar':
-        echo  $_POST['btn'];
-
-        break;
-
-    case 'buscar':
-        empty($_POST['cedula']) == 0 ? buscarid($_POST['cedula']) : print "debes digitar la cedula";
-        break;
+    if ($enviar) {
+        echo "se ah eliminado el usuario";
+    } else {
+        echo "el usuario no existe";
+    }
 }
+
+function Editarcliente($cedula)
+{
+
+    global $baseUrl;
+    $cliente = buscarid($cedula['cedula']);
+
+    $url = $baseUrl . "usuarios/" . $cliente['id'];
+
+    $options = array(
+        "http" => array(
+            "method" => "PUT",
+            "header" => "Content-type: application/json",
+            "content" => json_encode($cedula)
+        )
+    );
+    var_dump($options);
+
+    $context = stream_context_create($options);
+
+    $enviar = file_get_contents($url, false, $context);
+
+    if ($enviar) {
+        echo 'Se ha modificado correctamente el usuario';
+    } else {
+        echo "no se ha podido modificar el usuario";
+    }
+}
+
+
+
+
+if (empty($_POST['btn'])) {
+} else {
+    switch ($_POST['btn']) {
+
+        case 'borrar':
+            if (empty($_POST['cedula'])) {
+                echo "debes ingresar la cedula del usuario";
+            } else {
+                borrarCliente($_POST['cedula']);
+            }
+            break;
+
+        case 'guardar':
+            if (in_array("", $_POST)) {
+                echo "Debes completar todos los campos";
+            } else {
+                validarexistencia($_POST['cedula']) ?  print "el usuario ya existe" : crearUsuario($_POST);
+            }
+            break;
+        case 'editar':
+            echo  $_POST['btn'];
+            if (empty($_POST['cedula'])) {
+                echo "Debes completar todos los campos";
+            } else {
+                $nombretraer = Editarcliente($_POST);
+            }
+
+            break;
+
+        case 'buscar':
+            if (empty($_POST['cedula'])) {
+                echo "Debes ingresar la cedula";
+            } else {
+                $nombretraer =  buscarid($_POST['cedula']);
+            }
+            break;
+    }
+}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -148,20 +207,20 @@ switch ($_POST['btn']) {
     <div class="contenedor">
         <form action="index.php" method="POST">
             <div id="encabezado">
-                <input type="text" name="nombre" id="nombre" placeholder="nombre" value="<?php echo $nombretraer['nombre'] ?>">
+                <input type="text" name="nombre" id="nombre" placeholder="nombre" value="<?php isset($nombretraer['nombre']) ? print $nombretraer['nombre']  : print ""  ?>">
                 <img src="" alt="logo" srcset="https://media.licdn.com/dms/image/D563DAQFas8vErYi8iA/image-scale_191_1128/0/1681268367946?e=1686783600&v=beta&t=AxWQwwzzQ5dfcbg7RTf3_LAWFZUaEMWYY8pRfOr3MlM">
-                <input type="text" name="apellido" id="apellido" placeholder="apellido" value="<?php echo $nombretraer['apellido'] ?>">
-                <input type="number" name="edad" id="edad" placeholder="edad" value="<?php echo $nombretraer['edad'] ?>">
-                <input type="text" name="direccion" id="direccion" placeholder="direccion" value="<?php echo $nombretraer['direccion'] ?>">
-                <input type="text" name="email" id="email" placeholder="email" value="<?php echo $nombretraer['email'] ?>">
+                <input type="text" name="apellido" id="apellido" placeholder="apellido" value="<?php isset($nombretraer['apellido']) ? print $nombretraer['apellido']  : print "" ?>">
+                <input type="number" name="edad" id="edad" placeholder="edad" value="<?php isset($nombretraer['edad']) ? print $nombretraer['edad']  : print "" ?>">
+                <input type="text" name="direccion" id="direccion" placeholder="direccion" value="<?php isset($nombretraer['direccion']) ? print $nombretraer['direccion']  : print "" ?>">
+                <input type="text" name="email" id="email" placeholder="email" value="<?php isset($nombretraer['email']) ? print $nombretraer['email']  : print ""  ?>">
             </div>
             <div id="botonera">
 
-                <input type="time" name="Horario" id="Horario" placeholder="Horario de entrada" value="<?php echo $nombretraer['hora'] ?>">
+                <input type="time" name="hora" id="hora" placeholder="Horario de entrada" value="<?php isset($nombretraer['hora']) ? print $nombretraer['hora']  : print "" ?>">
 
-                <input type="text" name="team" id="team" placeholder="Team" value="<?php echo $nombretraer['team'] ?>">
-                <input type="text" name="trainer  " id="trainer" placeholder="Trainer" value="<?php echo $nombretraer['trainer'] ?>">
-                <input type="number" name="cedula" id="cedula" placeholder="cedula" value="<?php echo $nombretraer['cedula'] ?>">
+                <input type="text" name="team" id="team" placeholder="Team" value="<?php isset($nombretraer['team']) ? print $nombretraer['team']  : print "" ?>">
+                <input type="text" name="trainer  " id="trainer" placeholder="Trainer" value="<?php isset($nombretraer['trainer']) ? print $nombretraer['trainer']  : print "" ?>">
+                <input type="number" name="cedula" id="cedula" placeholder="cedula" value="<?php isset($nombretraer['cedula']) ? print $nombretraer['cedula']  : print "" ?>">
                 <div id="botones">
                     <button id="borrar" name="btn" value="borrar">&#10060;</button>
                     <button id="guardar" name="btn" value="guardar">&#10004;</button>
